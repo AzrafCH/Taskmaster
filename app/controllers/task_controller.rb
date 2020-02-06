2389815ef27dc479df7b374f36cc9ff8cfe25bbd
Azraf Chowdhury <achowdhur@ltu.edu>
2020-02-06 07:11:14 +0000

    added controllers

---
+++ app/controllers/task_controller.rb
@@ -0,0 +1,80 @@
+class TaskController < ApplicationController
+
+  get '/tasks' do
+    redirect_if_not_logged_in
+    @tasks = Task.all
+    erb :'/tasks/tasks'
+  end
+
+  get '/tasks/new' do
+    redirect_if_not_logged_in
+    erb :'/tasks/new'
+  end
+
+  post '/tasks' do
+    if logged_in?
+      if params["task"]["name"] == "" || params["task"]["content"] == ""
+      # env['x-rack.flash']flash[:notice] = "Please fill in all required fields."
+      session[:name] = params["task"]["name"]
+      session[:content] = params["task"]["content"]
+      flash.now[:notice] = "One of the required fields is blank"
+      erb :'/tasks/new'
+      else
+        @user = current_user
+        @task = Task.create(name: params["task"]["name"], content: params["task"]["content"], user_id: session[:user_id])
+       if @task.save
+         @user.tasks << @task
+          redirect to '/tasks'
+        else
+
+          redirect to '/tasks/new'
+       end
+     end
+    else
+      redirect to '/user/login'
+    end
+  end
+
+  get '/tasks/:id' do
+    redirect_if_not_logged_in
+    @task = Task.find_by(id: params[:id])
+    erb :'/tasks/show'
+  end
+
+  get '/tasks/:id/edit' do
+   redirect_if_not_logged_in
+    @task = Task.find_by(id: params[:id])
+    if @task && @task.user == current_user
+      erb :'/tasks/edit'
+    else
+      redirect to '/tasks'
+    end
+  end
+
+  patch '/tasks/:id' do
+  redirect_if_not_logged_in
+    if params["task"]["name"] == "" || params["task"]["content"] == ""
+      redirect to '/tasks/:id/edit'
+    else
+      @task = Task.find_by(id: params[:id])
+      if @task && @task.user == current_user
+        if @task.update(name: params["task"]["name"], content: params["task"]["content"])
+        #  env['x-rack.flash']flash[:notice] = "Your task has been updated."
+          redirect to '/tasks'
+        else
+          redirect to '/tasks'
+        end
+      end
+    end
+  end
+
+  delete '/tasks/:id/delete' do
+    redirect_if_not_logged_in
+    @task = Task.find_by(id: params["id"])
+      if @task && @task.user == current_user
+       @task.destroy
+      end
+  #  env['x-rack.flash']flash[:notice] = "Your task has been deleted."
+    redirect to '/tasks'
+  end
+end
---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,55 @@
+class UserController < ApplicationController
+  enable :sessions
+
+  get '/user/signup' do
+   if !logged_in?
+     erb :'/user/signup'
+   else
+     redirect to '/home'
+   end
+  end
+
+  post '/user/signup' do
+   if params[:username] == "" || params[:email] == "" || params[:password] == ""
+     redirect to '/user/signup'
+  elsif User.all.find { |user| user.username == params[:username]  or user.email == params[:email] }
+    flash[:error] = "This username or email is not available"
+    # env['x-rack.flash'][:notice] = "This username already belongs to another account" #have to add alerts, flash isnt working
+     redirect to '/user/signup'
+    else
+     @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+      @user.save
+      session[:user_id] = @user_id
+  # env['x-rack.flash'][:notice] = "You have succesfully registered!"
+      redirect to '/user/login'
+   end
+ end
+
+  get '/user/login' do
+    if !logged_in?
+      erb :'/user/login'
+    else
+      redirect to '/tasks'
+    end
+  end
+
+  post '/user/login' do
+    user = User.find_by(:username => params[:username])
+   if user && user.authenticate(params[:password]) && user.password == nil
+     session[:user_id] = user.id
+      redirect to '/user/login'
+    else
+      redirect to '/'
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.destroy
+     #env['x-rack.flash'][:notice] = "You have succesfully logged out!"
+      redirect to '/'
+    else
+      redirect '/'
+    end
+  end
+end
