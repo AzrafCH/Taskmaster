7d84fc545f47b303a97a579b74ba9386522b52ae
Azraf Chowdhury <achowdhur@ltu.edu>
2020-02-17 00:19:58 +0000

    added category controller actions

--- app/controllers/catergories_controller.rb
+++ app/controllers/catergories_controller.rb
@@ -1,12 +1,49 @@
 class CatergoriesController < ApplicationController
 
-  get '/category' do
-    @user = current_user
-    redirect_if_not_logged_in
-    @categories = Category.all
-    erb :'/categories/categories'
-  end
+  get '/categories' do
+      @category = Category.all
+      erb :'/categories/categories'
+    end
+
+    get '/categories/new' do
+      @tasks = Task.all
+      erb :'/categories/new'
+    end
+
+    post '/categories' do
+      @category = Category.create(params[:name])
+      if !params["task"]["name"].empty?
+        @category.tasks << Task.create(name: params["task"]["name"], content: params["task"]["content"])
+        # When using the shovel operator, ActiveRecord instantly fires update SQL
+        # without waiting for the save or update call on the parent object,
+        # unless the parent object is a new record.
+      end
+      redirect "/categories/#{@category.id}"
+    end
+
+    get '/categories/:id/edit' do
+      @category = Category.find(params[:id])
+      @tasks = Task.all
+      erb :'/categories/edit'
+    end
+
+    get '/categories/:id' do
+      @category = Category.find(params[:id])
+      erb :'/categories/show'
+    end
+
+    patch '/categories/:id' do
+      @category = Category.find(params[:id])
+      @category.update(params[:name])
+
+      if !params["task"]["name"].empty?
+        @category.tasks << Task.create(name: params["task"]["name"], content: params["task"]["content"])
+      end
+
+      redirect "/categories/#{@category.id}"
+    end
+
 
 end
 
-#I have to add categories, in which tasks get placed into a certain category. 
+#I have to add categories, in which tasks get placed into a certain category.
